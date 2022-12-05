73c135a9f56595108c9e67dc9f2262b05e93061a
KEPOCUH <tolik2001323@yandex.ru>
2022-12-05 23:59:23 +0300

    version 0.0.9.3.7.6.2
    Fixed Drug&Drop script.
    
    Now the widget cannot be moved outside the screen

--- Assets/Scripts/Various/DragAndDrop.cs
+++ Assets/Scripts/Various/DragAndDrop.cs
@@ -6,7 +6,7 @@ public class DragAndDrop : MonoBehaviour
     private bool canMove;
     private bool dragging;
     private RectTransform rect;
-    
+
     private Vector3 differenceBetweenRectAndClick;
     private Vector3 target;
 
@@ -21,7 +21,7 @@ public class DragAndDrop : MonoBehaviour
     {
 
 
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Input.GetMouseButton(0);
             Debug.Log("Зажатие мыши на перетаскиваемом объекте.");
@@ -32,18 +32,55 @@ public class DragAndDrop : MonoBehaviour
             dragging = true;
         }
 
-        if(dragging)
+        if (dragging)
         {
             //differenceBetweenRectAndClick = Input.mousePosition - rect.transform.position;
-            
+
+            Debug.Log("Screen: width=" + Screen.width + "; height= " + Screen.height);
             Debug.Log("Difference: " + differenceBetweenRectAndClick);
             Debug.Log("MousePos: " + Input.mousePosition);
             Debug.Log("RectPos: " + rect.transform.position);
             target = Input.mousePosition - differenceBetweenRectAndClick;//+ new Vector3(rect.sizeDelta.x / 2,rect.sizeDelta.y / 2,0) ;
-            rect.transform.position = target;
+            Debug.Log(("Target = " + target).SetColor(Color.Yellow));
+
+            if (rect.pivot.x >= 0.5f)
+            {
+                if (rect.pivot.y >= 0.5f)
+                {
+                    if (target.x > Screen.width || target.x - rect.sizeDelta.x < 0)
+                    {
+                        if (target.y > Screen.height || target.y - rect.sizeDelta.y < 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            rect.transform.position = new Vector3(rect.transform.position.x, target.y);
+                        }
+                    }
+                    else if(target.y > Screen.height || target.y - rect.sizeDelta.y < 0)
+                    {
+                        if (target.x > Screen.width || target.x - rect.sizeDelta.x < 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            rect.transform.position = new Vector3(target.x, rect.transform.position.y);
+                        }
+                    }
+                    else
+                    {
+                        rect.transform.position = target;
+                    }
+                }
+            }
+
+
+            //rect.transform.position = target;
         }
 
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))
         {
             canMove = false;
             dragging = false;
